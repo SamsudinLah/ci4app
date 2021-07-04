5b3b58d79ffcb39bb5cf61a3b7d4f840baa1f062
SamsudinLah <samsudinlah94@gmail.com>
2021-07-04 14:57:12 +0700

    Tambah Modal Update Multiple

--- app/Views/orang/index.php
+++ app/Views/orang/index.php
@@ -6,6 +6,7 @@
         <div class="col-6">
             <!-- judul halaman -->
             <h1 class="mt-2">Daftar Orang</h1>
+            <!-- TOMBOL MODAL -->
             <form action="" method="post">
                 <!-- pencarian -->
                 <div class="input-group mb-3">
@@ -15,6 +16,47 @@
             </form>
         </div>
     </div>
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalcreate">
+        Tambah Orang
+    </button>
+
+    <!-- Modal Tambah -->
+    <div class="modal fade" id="modalcreate" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Tambah Orang</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="/orang/save/" method="post" enctype="multipart/form-data">
+                        <?= csrf_field(); ?>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Nama</label>
+                            <input type="text" value="" class="form-control" id="exampleInputEmail1" name="nama[]" aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Alamat</label>
+                            <input type="text" value="" name="alamat[]" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Nama</label>
+                            <input type="text" value="" class="form-control" id="exampleInputEmail1" name="nama[]" aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Alamat</label>
+                            <input type="text" value="" name="alamat[]" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="row">
         <div class="col">
             <!-- tabel daftar komik -->
